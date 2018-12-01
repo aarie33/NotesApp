0b15873b4fc1ed2813f6749cc2647f1065998439
Ahmad Syamsul Arifin <aarie33ahmad@gmail.com>
2018-12-02 00:03:36 +0700

    Content provider

--- app/src/main/java/arie/notesapp/MainActivity.java
+++ app/src/main/java/arie/notesapp/MainActivity.java
@@ -1,6 +1,7 @@
 package arie.notesapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,19 +12,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 import static arie.notesapp.FormAddUpdateActivity.REQUEST_UPDATE;
+import static arie.notesapp.db.DatabaseContract.CONTENT_URI;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     RecyclerView rvNotes;
     ProgressBar progressBar;
     FloatingActionButton fabAdd;
 
-    private LinkedList<Note> list;
+    private Cursor list;
     private NoteAdapter adapter;
-    private NoteHelper noteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +37,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         progressBar = (ProgressBar)findViewById(R.id.progressbar);
         fabAdd = (FloatingActionButton)findViewById(R.id.fab_add);
         fabAdd.setOnClickListener(this);
-
-        noteHelper = new NoteHelper(this);
-        noteHelper.open();
-
-        list = new LinkedList<>();
 
         adapter = new NoteAdapter(this);
         adapter.setListNotes(list);
@@ -66,32 +59,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private class LoadNoteAsync extends AsyncTask<Void, Void, ArrayList<Note>> {
+    private class LoadNoteAsync extends AsyncTask<Void, Void, Cursor>{
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             progressBar.setVisibility(View.VISIBLE);
 
-            if (list.size() > 0){
-                list.clear();
-            }
         }
 
         @Override
-        protected ArrayList<Note> doInBackground(Void... voids) {
-            return noteHelper.query();
+        protected Cursor doInBackground(Void... voids) {
+            return getContentResolver().query(CONTENT_URI,null,null,null,null);
         }
 
         @Override
-        protected void onPostExecute(ArrayList<Note> notes) {
+        protected void onPostExecute(Cursor notes) {
             super.onPostExecute(notes);
             progressBar.setVisibility(View.GONE);
 
-            list.addAll(notes);
+            list = notes;
             adapter.setListNotes(list);
             adapter.notifyDataSetChanged();
 
-            if (list.size() == 0){
+            if (list.getCount() == 0){
                 showSnackbarMessage("Tidak ada data saat ini");
             }
         }
@@ -100,11 +90,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == FormAddUpdateActivity.REQUEST_ADD){
             if (resultCode == FormAddUpdateActivity.RESULT_ADD){
                 new LoadNoteAsync().execute();
                 showSnackbarMessage("Satu item berhasil ditambahkan");
-                // rvNotes.getLayoutManager().smoothScrollToPosition(rvNotes, new RecyclerView.State(), 0);
             }
         }
         else if (requestCode == REQUEST_UPDATE) {
@@ -112,15 +102,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (resultCode == FormAddUpdateActivity.RESULT_UPDATE) {
                 new LoadNoteAsync().execute();
                 showSnackbarMessage("Satu item berhasil diubah");
-                // int position = data.getIntExtra(FormAddUpdateActivity.EXTRA_POSITION, 0);
-                // rvNotes.getLayoutManager().smoothScrollToPosition(rvNotes, new RecyclerView.State(), position);
             }
 
             else if (resultCode == FormAddUpdateActivity.RESULT_DELETE) {
-                int position = data.getIntExtra(FormAddUpdateActivity.EXTRA_POSITION, 0);
-                list.remove(position);
-                adapter.setListNotes(list);
-                adapter.notifyDataSetChanged();
+                new LoadNoteAsync().execute();
                 showSnackbarMessage("Satu item berhasil dihapus");
             }
         }
@@ -129,9 +114,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (noteHelper != null){
-            noteHelper.close();
-        }
     }
 
     private void showSnackbarMessage(String message){
--- app/src/main/java/arie/notesapp/provider/NoteProvider.java
+++ app/src/main/java/arie/notesapp/provider/NoteProvider.java
@@ -1,4 +1,4 @@
-package arie.notesapp;
+package arie.notesapp.provider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
+import arie.notesapp.NoteHelper;
 import arie.notesapp.db.DatabaseContract;
 
 import static arie.notesapp.db.DatabaseContract.AUTHORITY;
@@ -20,11 +21,7 @@ public class NoteProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     static {
-
-        // content://com.dicoding.mynotesapp/note
         sUriMatcher.addURI(AUTHORITY, DatabaseContract.TABLE_NOTE, NOTE);
-
-        // content://com.dicoding.mynotesapp/note/id
         sUriMatcher.addURI(AUTHORITY,
                 DatabaseContract.TABLE_NOTE+ "/#",
                 NOTE_ID);
